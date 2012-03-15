33f59e6fb304735c001cf9c6c4c390f5422ff6bf
alvarodelrosal <alvarodelrosal@gmail.com>
2012-03-15 12:40:07 +0000

    Agregado el Render para los nombres de archivo.

--- ftp-client/src/alvarodelrosal/ftp/ui/ModeloDeTablaDeArchivos.java
+++ ftp-client/src/alvarodelrosal/ftp/ui/ModeloDeTablaDeArchivos.java
@@ -5,6 +5,8 @@ import alvarodelrosal.ftp.modelo.Path;
 import alvarodelrosal.ftp.modelo.RepositorioDePaths;
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.table.AbstractTableModel;
 
 public class ModeloDeTablaDeArchivos extends AbstractTableModel {
